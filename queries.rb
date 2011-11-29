c69bca45056d9cff60a280a39597c08dd314a61e
5t0rm <storm@5T0RM-R700.(none)>
2011-11-29 16:22:37 +0100

    Added queries.

---
+++ queries.rb
@@ -0,0 +1,37 @@
+require 'Telnet'
+
+class Query
+  def version
+    Telnet.new.query "V"
+  end
+  
+  def listAirlines
+    Telnet.new.query "A"
+  end
+  
+  def listAirports
+    Telnet.new.query "P"
+  end
+  
+  def listDestinations(airport)
+    Telnet.new.query "D" + airport
+  end
+  
+  def listConnections(date, source, destination)
+    Telnet.new.query "C" + source + destination + date
+  end
+  
+  def listLocations(flight)
+    Telnet.new.query "F" + flight
+  end
+  
+  def listFlightDays(flight)
+    Telnet.new.query "W" + flight 
+  end
+  
+  def listSeats(date, flight, type)
+    Telnet.new.query "S" + date + flight + type
+  end
+end
+
+Query.new.listDestinations "TEL"
