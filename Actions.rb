840b6c4b3708d4b5307dacc172083d27ba269a25
rdebruyne <mail@debruyne.me>
2011-11-29 16:39:59 +0100

    actions

--- Actions.rb
+++ Actions.rb
@@ -1,9 +1,10 @@
 require 'Telnet'
+require 'Util'
 
 class Actions
 
 	def holding(date,flightnumber,klasse,gender,firstname,surname)
-		Telnet.new.query "H"+date+flightnumber+klasse+gender+firstname+surname
+		Telnet.new.query "H"+date+flightnumber+klasse+gender+(Util.new.StringValidate firstname,15)+(Util.new.StringValidate surname,20)
 	end
 
 end
--- basicClient.rb
+++ basicClient.rb
@@ -1,3 +1,7 @@
 require 'Actions'
+require 'Util'
 
-Actions.new.holding "2011-11-05","AFR001","B","M","Dave           ","Clarke              "
+
+Actions.new.holding "2011-11-05","AFR001","B","M","Dave","Clarke"
+
+
