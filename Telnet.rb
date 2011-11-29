f797f12306b79be8f19e8ac3cc0b8807a668496f
rdebruyne <mail@debruyne.me>
2011-11-29 17:59:31 +0100

    telnet

--- Telnet.rb
+++ Telnet.rb
@@ -5,11 +5,14 @@ class Telnet
 def query(input)
   host = Net::Telnet.new('Host' => 'localhost', 'Port' => 12111)
   host.puts(input)
+  result = []
   while line = host.gets
-    puts line
+    result << line
   end
   host.flush
   host.close
+  
+  return result
 end
 
 end
