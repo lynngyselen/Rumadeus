cb2a44110fceb62be681ae6fb5e5b71149a4b328
rdebruyne <mail@debruyne.me>
2011-11-29 18:08:19 +0100

    commit

--- Actions.rb
+++ Actions.rb
@@ -4,7 +4,7 @@ require 'Util'
 class Actions
 
 	def hold(date,flightnumber,klasse,gender,firstname,surname)
-		Telnet.new.query "H"+(Util.new.LengthCheck date,10)
+		return Telnet.new.query "H"+(Util.new.LengthCheck date,10)
                         +(Util.new.LengthCheck fligthnumber,6)
                         +(Util.new.LengthCheck klasse,1)
                         +(Util.new.LengthCheck gender,1)
@@ -13,15 +13,15 @@ class Actions
   end
 	
 	def book(code)
-	  Telnet.new.query "B"+(Util.new.LengthCheck code,32)
+	 return Telnet.new.query "B"+(Util.new.LengthCheck code,32)
 	end
 
   def cancel(code)
-    Telnet.new.query "X"+(Util.new.LengthCheck code,32)
+    return Telnet.new.query "X"+(Util.new.LengthCheck code,32)
   end
 
   def query(code)
-    Telnet.new.query "Q"+(Util.new.LengthCheck code,32)
+    return Telnet.new.query "Q"+(Util.new.LengthCheck code,32)
   end
 
 end
--- basicClient.rb
+++ basicClient.rb
@@ -1,10 +1,11 @@
 require 'Actions'
 require 'Util'
-
+require 'queries'
 
 #Actions.new.holding "2011-11-05","AFR001","B","M","Dave","Clarke"
 
+#puts Actions.new.query "3f4cdf33a688ae1e126e16e42e2a2f04"
 
-#Actions.new.book "3f4cdf33a688ae1e126e16e42e2a2f04"
+puts Query.new.listConnections "2012-01-31","BRU","CDG"
 
-Actions.new.cancel "3f4cdf33a688ae1e126e16e42e2a2f04"
+puts Query.new.listDestinations "AMS"
