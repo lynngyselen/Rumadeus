c227d54631e10795b2416b85bf8bb71cba94ad73
rdebruyne <mail@debruyne.me>
2011-12-02 09:36:58 +0100

    m

--- Objects.rb
+++ Objects.rb
@@ -12,7 +12,7 @@ class Airline
   def initialize(input)
     input = Util.new.LengthCheck(input,33)
     @code = Code.new(input[0,2])
-    @name = input.sub[3,32]
+    @name = input[3,32]
   end
 end
 
@@ -21,8 +21,26 @@ class Airport
   def initialize(input)
     input = Util.new.LengthCheck(input,43)
     @code = Code.new(input[0,2])
-    @city = input.sub[3,22]
-    @country = input.sub[23,42]    
+    @city = input[3,22]
+    @country = input[23,42]    
+  end
+end
+
+class Time
+  def initialize(input)
+    input = Util.new.LengthCheck(input,4)
+    @hour = input[0,1]
+    @minute = input[2,3]
+  end
+end
+
+class Connection
+  
+  def initialize(input)
+    input = Util.new.LengthCheck(input,16)
+    @flightnr = input[0,5]
+    @deptime = input[6,10]
+    @duration = input[11,15]     
   end
 end
 
