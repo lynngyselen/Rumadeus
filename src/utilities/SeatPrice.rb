60b298c8910d84d98d740ab45286b50a545aa984
rdebruyne <mail@debruyne.me>
2011-12-05 19:16:33 +0100

    bestprice

--- src/utilities/SeatPrice.rb
+++ src/utilities/SeatPrice.rb
@@ -1,9 +1,23 @@
 class SeatPrice
-  
+  include Comparable
   def initialize(input)
     input =~ /(\d{4})(\d{4})/
     @seats = $1
     @price = $2
+  end
+  
+    def <=>(other)
+    if self.size < other.getPrice
+      -1
+    elsif self.size > other.getPrice
+      1
+    else
+      0
+    end
+  end
+  
+  def getPrice
+    @price
   end
   
   def to_s
