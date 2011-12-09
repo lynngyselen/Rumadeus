d3d4196693d141e29c9341f44ec448ffa44a8882
RamsesDeNorre <ramses.rommel@gmail.com>
2011-12-09 17:00:42 +0100

    Rename flightnr to flightCode because it is not a number.

--- src/utilities/Connection.rb
+++ src/utilities/Connection.rb
@@ -4,17 +4,17 @@ class Connection
   
   def initialize(input)
     input =~ /(\w{3}\d{3})([0-9:]{5})([0-9:]{5})/
-    @flightnr = $1
+    @flightCode = $1
     @deptime = Time.new($2)
     @duration = Time.new($3)   
   end
   
   attr_accessor :date, :departure, :arrival
-  attr_reader :flightnr 
+  attr_reader :flightCode 
   
   def to_s
     (@date.to_s) + " " + (@departure.to_s) + " " + (@arrival.to_s) + " " + 
-      (@flightnr.to_s) + " " + (@deptime.to_s) + " " + (@duration.to_s)
+      (@flightCode.to_s) + " " + (@deptime.to_s) + " " + (@duration.to_s)
   end
   
 end
