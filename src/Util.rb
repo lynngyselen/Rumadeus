19d6f93eff381ce5b3317176b38f3b5ff159192f
RamsesDeNorre <ramses.rommel@gmail.com>
2011-12-09 18:03:31 +0100

    Formatting in Util.

--- src/Util.rb
+++ src/Util.rb
@@ -1,10 +1,10 @@
 module Util
 
   def Util::stringValidate(str, max)
-    if(str.length > max)
+    if str.length > max
       raise InvalidInputException, "input too long..."
     else
-      for i in (1 .. (max - str.length))
+      for i in 1 .. (max - str.length)
         str += " "
       end
     end
@@ -12,7 +12,7 @@ module Util
   end
 
   def Util::lengthCheck(str, size)
-    if(str.length != size)
+    if str.length != size
       raise InvalidInputException, "input error: the string \"#{str}\" "+
         "has size #{str.length} instead of #{size}."
     end
@@ -30,22 +30,22 @@ module Util
     ERRNR = "ERRNR"
     ERRXX = "ERRXX"
     
-    def initialize (errorMsg)
+    def initialize errorMsg
       @errorMsg = errorMsg
     end
     
     def cause
       case @errorMsg
-      when ERRIM
-        "#{ERRIM}: Ill formatted message."
-      when ERREC
-        "#{ERREC}: Empty command."
-      when ERRNR
-        "#{ERRNR}: No results."
-      when ERRXX
-        "#{ERRXX}: Something went wrong badly..."
-      else
-        "Unknown error code."
+        when ERRIM
+          "#{ERRIM}: Ill formatted message."
+        when ERREC
+          "#{ERREC}: Empty command."
+        when ERRNR
+          "#{ERRNR}: No results."
+        when ERRXX
+          "#{ERRXX}: Something went wrong badly..."
+        else
+          "Unknown error code."
       end
     end
   end
