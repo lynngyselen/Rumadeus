a566e7472cd03102e9bed6510584bc4cb136a809
RamsesDeNorre <ramses.rommel@gmail.com>
2011-12-12 19:49:15 +0100

    Made clear that delegate should always be implemented by AbstractQuery subclasses.

--- src/AbstractQuery.rb
+++ src/AbstractQuery.rb
@@ -6,6 +6,8 @@ class AbstractQuery
     @telnet = Telnet.new
   end
   
+  # Subclasses should make sure to implement the delegate method,
+  # otherwise we will probably get an infinite loop here
   def method_missing *args
     delegate.public_send *args
   end
