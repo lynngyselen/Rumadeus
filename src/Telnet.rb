0f247bd54568303aca75f7087eabec0ce134b7ae
RamsesDeNorre <ramses.rommel@gmail.com>
2011-12-12 15:36:32 +0100

    Refactor the Telnet class to be more structured.

--- src/Telnet.rb
+++ src/Telnet.rb
@@ -2,46 +2,55 @@ require 'net/telnet'
 
 class Telnet
 
-  # Newer Ruby does not have a gets method in Telnet anymore, use cmd instead.
   def query input
-    host = Net::Telnet.new('Host' => 'localhost', 'Port' => 12111)
+    host = init_connection
     
-    result = if host.respond_to? :cmd then
-        newQuery(input, host)
-      else
+    handle_result host.respond_to?(:cmd) ?
+        newQuery(input, host) :
         oldQuery(input, host)
-      end
-        
-    if not result.empty? and (result.at 0).start_with? "ERR"
-      raise Util::ServerError.new (result.at 0)
-    end
-    
-    # Return an empty array instead of nil when the query has no results.
-    result || []
   end
   
-  # With command, the reply consists of a single multi-line string.
-  # We convert this to an array of lines.
+  def init_connection
+    Net::Telnet.new('Host' => 'localhost', 'Port' => 12111)
+  end
+  
+  def handle_result result
+    if error? result
+      raise Util::ServerError.new (result.at 0)
+    else
+      result | [] # Return an empty array instead of nil
+    end
+  end
+  
+  def error? input
+    not input.empty? and (input.at 0).start_with? "ERR"
+  end
+  
+  # Newer Ruby does not have a gets method in Telnet anymore, use cmd instead.
   def newQuery(input, host)
     output = host.cmd input
     close host
-    output.nil? ? [] : output.split("\n")
+    if output.nil? then [] else output.split("\n") end
   end
   
+  # For older Ruby versions
   def oldQuery(input, host)
     results = []
     host.puts input
     while line = host.gets
       results << line[0, line.length - 1]
     end
-    host.flush
-    host.close
+    close host
     results
   end
   
+  # Find out at runtime which cleanup procedures the host is familiar with,
+  # this has changed even between different 1.9.x versions, it seems
   def close host
-    if host.respond_to? "close"
-      host.close
+    [:flush, :close].each do |op|
+      if host.respond_to? op
+        host.send op
+      end
     end
   end
   
