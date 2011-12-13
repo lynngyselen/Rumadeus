950aca2340b6ed0698590bc02625f77fd044a1e5
RamsesDeNorre <ramses.rommel@gmail.com>
2011-12-13 21:24:24 +0100

    Improved Actions.rb

--- src/Actions.rb
+++ src/Actions.rb
@@ -11,32 +11,43 @@ class Actions < AbstractQuery
   end
 
 	def hold(date, flightnumber, klasse, gender, firstname, surname)
-		@telnet.query "H"
-		  + (Util::lengthCheck date, 10)
-      + (Util::lengthCheck fligthnumber, 6)
-      + (Util::lengthCheck klasse, 1)
-      + (Util::lengthCheck gender, 1)
-      + (Util::stringValidate firstname, 15)
-      + (Util::stringValidate surname, 20)
+	  query = "H" +
+      (Util::lengthCheck date, 10) +
+      (Util::lengthCheck flightnumber, 6) +
+      (Util::lengthCheck klasse, 1) +
+      (Util::lengthCheck gender, 1) +
+      (Util::stringValidate firstname, 15) +
+      (Util::stringValidate surname, 20)
+		result = @telnet.query query
+		if result.length > 0 and (result.at 0).start_with? "S"
+		  "Success: #{[(result.at 0)[1..(result.at 0).length]]}"
+		else
+		  result
+		end
   end
 	
 	def book(code)
 	  result = []
-    (@telnet.query "B" + (Util::lengthCheck code, 32)).each { |r|
+    (@telnet.query ("B" + (Util::lengthCheck code, 32))).each do |r|
       result << Booking.new(r)
-    }
+    end
     result
 	end
 
   def cancel(code)
-    @telnet.query "X" + (Util::lengthCheck code, 32)
+    result = @telnet.query ("X" + (Util::lengthCheck code, 32))
+    if result.include? "S"
+      ["Success"]
+    else
+      result
+    end
   end
 
   def query(code)
     result = []
-    (@telnet.query "Q" + (Util::lengthCheck code, 32)).each { |r|
+    (@telnet.query ("Q" + (Util::lengthCheck code, 32))).each do |r|
       result << Booking.new(r)
-    }
+    end
     result
   end
   
