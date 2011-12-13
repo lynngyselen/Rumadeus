5054a09b91897841cd6d1685882cd84ec2c2c66a
RamsesDeNorre <ramses.rommel@gmail.com>
2011-12-13 21:09:34 +0100

    Revert "Don't shoot me, de tests zouden niet mogen..." See email.
    This reverts commit f41cf47de2174774c9e149740fec13b27321b089.

--- src/Actions.rb
+++ src/Actions.rb
@@ -5,64 +5,41 @@ require 'utilities/Booking'
 
 
 class Actions < AbstractQuery
-   
+    
   def initialize
     super
   end
 
-    def hold(date, flightnumber, klasse, gender, firstname, surname)
-      holds = []     
-        (@telnet.query "H"
-          + (Util::lengthCheck date, 10)
+	def hold(date, flightnumber, klasse, gender, firstname, surname)
+		@telnet.query "H"
+		  + (Util::lengthCheck date, 10)
       + (Util::lengthCheck fligthnumber, 6)
       + (Util::lengthCheck klasse, 1)
       + (Util::lengthCheck gender, 1)
       + (Util::stringValidate firstname, 15)
-      + (Util::stringValidate surname, 20)).each { |h|
-        if succes? h
-          holds << Holding.new(h)
-        elsif (h include? "FN") then raise "No seat available in specified category" 
-        end
-      }
-      holds
+      + (Util::stringValidate surname, 20)
   end
-   
-    def book(code)
-      booking = []
+	
+	def book(code)
+	  result = []
     (@telnet.query "B" + (Util::lengthCheck code, 32)).each { |r|
-      if succes? r
-      booking << Booking.new(r)
-        elsif (r include? "FN") then raise "No booking available"
-        elsif (r include? "FA") then raise "Seat already booked"  
-        end 
+      result << Booking.new(r)
     }
-    booking
-    end
+    result
+	end
 
   def cancel(code)
-    (@telnet.query "X" + (Util::lengthCheck code, 32)).each { |c|
-      if succes?
-        elsif (c include? "FN") then raise "Invalid booking code"
-        elsif (c include? "FA") then raise "Booking is older than 24h and can't be cancelled"  
-        end 
-    }
+    @telnet.query "X" + (Util::lengthCheck code, 32)
   end
 
   def query(code)
-    booking = []
+    result = []
     (@telnet.query "Q" + (Util::lengthCheck code, 32)).each { |r|
-      if succes? r
-      booking << Booking.new(r)
-      elsif (r include? "FN") then raise "No holding or booking"
-      end 
+      result << Booking.new(r)
     }
-    booking
+    result
   end
- 
-  def succes?(output)
-    (output include? "S") ? true : false
-  end
- 
+  
   # The chain of command can be extended by overriding or monkey patching this
   # method to insert any other class.
   # The extendor is responsible for proper handling of
